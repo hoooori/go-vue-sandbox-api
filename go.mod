184f45216d91afb5e52d358fd741e32e8045a93c
hoooori <hideyasu.horiuchi@gmail.com>
2020-05-13 22:32:22 +0900

    installed gin

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module go-vue-sandbox-api
 
 go 1.13
+
+require github.com/gin-gonic/gin v1.6.3 // indirect
---
+++ main.go
@@ -0,0 +1,19 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	// router定義
+	r := gin.Default()
+
+	r.GET("/ping", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "pong",
+		})
+	})
+
+	r.Run()
+	// r.Run(":8080")
+}
